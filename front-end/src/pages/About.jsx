ec7fa84284310bc1c9857edebc9c5fe0e89038e8
KChoi0720 <choicanlight@gmail.com>
2025-04-02 19:02:33 +1300

    edit about/contact/login/myprofile page

--- front-end/src/pages/About.jsx
+++ front-end/src/pages/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      About Page
+      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>About <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt='' />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to medical centre. Your trusted partner in managing your healthcare conveniently and efficiently. At medical centre, we understood
+            the importance of providing exceptional healthcare services to our patients. 
+          </p>
+          <p>Medical center is commited to excellence in healthcare technology. We continuously strive to enhance our platform, integrating hte latest advancements to improve
+            user experience and deliver superior service. Whether you're booking your first appintment or managing ongoing care, we are here to support you every step of the way.
+          </p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision at medical center is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you
+            to access the care you need, when you need it.
+          </p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-900 hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiently:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-900 hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convinience:</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-900 hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
--- front-end/src/pages/MyProfile.jsx
+++ front-end/src/pages/MyProfile.jsx
@@ -1,9 +1,38 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+
+  const [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image:assets.profile_pic,
+    email:'jiping@gmail.com',
+    phone: '+64 123-123-123',
+    address:{
+      line1:"111 Mount Eden Road",
+      line2:"Mount Eden, Auckland"
+    },
+    gender:'Male',
+    dob:'2024-02-02'
+  })
+
+
+  const [isEdit, setIsEdit] = useState(false)
+
   return (
     <div>
-      Profile Page
+      
+      <img src={assets.profile_pic} alt='' />
+      
+      {
+        isEdit
+        ? <input type='text' value={userData.name} onChange={e => setUserData(prev => ({...prev, name:e.target.value}))}/>
+        : <p>{userData.name}</p>
+      }
+
     </div>
   )
 }
