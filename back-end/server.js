19efbc3915a68be8aec199f51aa209f46d41dbbc
KChoi0720 <choicanlight@gmail.com>
2025-04-03 22:56:57 +1300

    delete related files

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,24 @@
 import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+ 
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+
+// middlewares
+app.use(express.json()) //request bypass with this
+app.use(cors()) // allow front end to connect
+
+
+// api endpoints
+app.get('/', (req, res) => {
+    res.send('api working')
+})
+
+app.listen(port, () => {
+    console.log('server started', port)
+})
+
 
 
