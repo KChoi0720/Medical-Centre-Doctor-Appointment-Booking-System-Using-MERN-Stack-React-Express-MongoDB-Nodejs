64052ccfab3fc1848ed88eed659f385bffe7bf93
KChoi0720 <choicanlight@gmail.com>
2025-04-04 00:24:48 +1300

    update admin and doctor controller

---
+++ back-end/models/userModel.js
@@ -0,0 +1,21 @@
+// used to store user's data in the database
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  // image: {
+  //   type: String,
+  //   default:""  }, // use image to datebase 64 to transfer
+  address: { type: Object, required: { line1: "", line2: "" } },
+  gender: { type: String, default: "Not Selected" },
+  dob: { type: String, default: "Not Selected" },
+  phone: { type: String, default: "000000000" },
+});
+
+const userModel =
+  mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
  
 // app config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+
 
 // middlewares
 app.use(express.json()) //request bypass with this
@@ -12,9 +19,18 @@ app.use(cors()) // allow front end to connect
 
 
 // api endpoints
+
+app.use('/api/admin', adminRouter)
+// localhost:4000/api/admin/add-doctor
+
+
 app.get('/', (req, res) => {
     res.send('api working')
 })
+
+
+
+
 
 app.listen(port, () => {
     console.log('server started', port)
