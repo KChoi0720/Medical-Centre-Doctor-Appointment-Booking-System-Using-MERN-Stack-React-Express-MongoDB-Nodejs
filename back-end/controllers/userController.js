c4c95bab45c83245b05f32d99fb648e4e6227ac1
KChoi0720 <choicanlight@gmail.com>
2025-04-05 19:25:51 +1300

    testing api updating doctor-user profiles

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -112,4 +113,54 @@ const getProfile = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser, getProfile };
+
+// API to update user profile
+
+const updateProfile = async (req, res) => {
+    try {
+
+        const { userId, name, phone, address, dob, gender } = req.body
+        const imageFile = req.file
+        
+        if (!name || !phone || !address || !dob || !gender) {
+            return res.json({
+                success: false,
+                message: 'Data Missing'
+            })
+        } 
+
+        // This function can find the data with userId and update the profile parameters.
+        await userModel.findByIdAndUpdate(userId, {
+            name,
+            phone,
+            address: JSON.parse(address),// can help with google JSON to stringify
+            dob,
+            gender,
+        })
+
+        if (imageFile) {
+            
+            // upload image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: 'image'})
+            const imageURL = imageUpload.secure_url
+            
+            await userModel.findByIdAndUpdate(userId, {image: imageURL})
+        }
+
+        res.json({
+            success: true,
+            message: 'Profile updated successfully'
+        })
+      
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+      }
+}
+
+
+
+
+
+export { registerUser, loginUser, getProfile, updateProfile };
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -15,6 +15,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 
+
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
